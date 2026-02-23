9a2d264a776946e104852701ce07abd8080a34b1
M.A.MOID SUFIYAN <160425733054@mjcollege.ac.in>
2026-02-23 22:21:44 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,16 @@
+# nums = (1, 2, 33, 44, 55, 66, 77, 88, 99)
+# x = 33
+# index = 0
+# for el in nums:
+#    if el == x:
+#       print(f"Found {x} at index {index}")
+#       break
+#    index += 1
+# else:
+#    print(f"{x} not found in the tuple")
+
+# for i in range(100, 1,-1):
+#     print(i)
+# ==============================================
+for i in range(1, 101):
+    pass
