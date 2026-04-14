0e14c5b689349e67c75676f18af93e79d25e224e
M.A.MOID SUFIYAN <160425733054@mjcollege.ac.in>
2026-04-14 12:16:09 +0530

    Add files via upload

---
+++ 1ch-1.py
@@ -0,0 +1,3 @@
+first_number = int(input("Enter number a :"))
+second_number = int(input("Enter number b :"))
+print ("sum = ",first_number + second_number)
---
+++ 1ch-3.py
@@ -0,0 +1,3 @@
+num1 = float(input("enter the first number :"))
+num2 = float(input("enter the second number :"))
+print ("the avg of two nums are", (num1 + num2) / 2)
